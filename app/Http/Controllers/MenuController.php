8141ff2419d059fa485fe157524d1ac54561616f
Arindam Pal <arindam.pal024@gmail.com>
2022-06-15 21:29:38 +0530

    Menu controller also fixed

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -95,6 +95,9 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
+        return MenuItem::with('children')
+            ->whereNull('parent_id')
+            ->get();
         throw new \Exception('implement in coding task 3');
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    public function children()
+    {
+        return $this->hasMany(Self::class, 'parent_id', 'id')
+            ->with('children');
+    }
+
 }
