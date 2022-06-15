6076aeb347d062f9a3c8e131ee91b2b150193582
Arindam Pal <arindam.pal024@gmail.com>
2022-06-15 21:22:50 +0530

    Fixed all event related functions

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Event;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class EventsController extends BaseController
 {
@@ -101,7 +102,8 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        return Event::with('workshops')->get();
+        // throw new \Exception('implement in coding task 1');
     }
 
 
@@ -180,6 +182,11 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
+        return Event::with('workshops')
+            ->whereHas('workshops', function($q){
+                $q->where('start' , '>=' , Carbon::now()->toDateTimeString());
+            })
+            ->get();
         throw new \Exception('implement in coding task 2');
     }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 
+use App\Models\Workshop;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class, 'event_id', 'id');
+    }
+
 }
